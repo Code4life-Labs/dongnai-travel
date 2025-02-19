c3807e9cc89cb2f38fd04f33be5c3f2072df68b2
phapdev <luongphap1810@gmail.com>
2025-02-19 09:50:02 +0700

    fix androi package

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      ['@babel/plugin-transform-runtime', {
+        helpers: true,
+        regenerator: false
+      }] 
+    ]
   };
 };
