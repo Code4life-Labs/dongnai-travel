121d325906a576cfd50d2ff267f2cdefe1b52642
phapdev <luongphap1810@gmail.com>
2025-02-24 16:27:23 +0700

    fix androi

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
       ['@babel/plugin-transform-runtime', {
         helpers: true,
-        regenerator: false
-      }] 
+        regenerator: true
+      }],
+      'react-native-reanimated/plugin',
+      'expo-router/babel'
     ]
   };
 };
